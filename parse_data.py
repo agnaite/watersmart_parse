c909dbd34c00144446ead899415280c6acbfc98e
Agne Klimaite <agnaite@me.com>
2017-11-02 10:46:22 -0700

    refactored and added docstrings

--- parse_data.py
+++ parse_data.py
@@ -1,9 +1,10 @@
 from pprint import pprint
 from datetime import datetime
-import csv
 import regex
+import csv
 
-def get_input(fname):
+def parse_and_write_csv(fname):
+    """Reads in text file, parses data, and writes to file"""
 
     with open(fname) as f:
         data = f.readlines()
@@ -20,21 +21,24 @@ def get_input(fname):
                          'Consumption': regex.sub('[^0-9]','', row[-1]),
                          'Read Date': date,
                          'Zip Code': regex.sub('[^0-9]','', row[-2])})
-    return all_data
-
-def write_output(data):
 
     order = ["Account Number","Read Date","Address","Zip Code", "Consumption"]
+
+    write_output(all_data, order)
+
+##### helpers #####
+
+def write_output(data, order):
+    """Takes in a list of dictionaries, an order based on keys, and writes to a csv."""
 
     with open('file.csv', 'w') as output_file:
         dict_writer = csv.DictWriter(output_file, order)
         dict_writer.writeheader()
         dict_writer.writerows(data)
 
-
-##### helpers #####
-
 def get_address(text):
+    """Uses regex to get street address."""
+
     reg = regex.compile(r'\b[0-9]{1,3}(?:\s\p{L}+)+')
     results = reg.search(text)
 
@@ -44,6 +48,11 @@ def get_address(text):
         return None
 
 def get_date(text):
+    """Uses regex to get date in format like 'Mar 2 2017' and reformats to
+       YYYYMMDD.
+
+    """
+
     reg = regex.compile(r'\w{3}[ ]\d{1,2}[ ]\d{4}')
     results = reg.search(text)
 
@@ -53,4 +62,4 @@ def get_date(text):
     else:
         return None
 
-write_output(get_input('example_input.txt'))
+parse_and_write_csv('example_input.txt')
